6b28c2ef70afb976a6b23b83770df77e56779858
Xalcon <xalcon@xalcon.net>
2017-05-20 06:37:48 +0200

    changed tooltip to strike through currently unused modifiers fixed gradle config

--- src/main/java/net/xalcon/analyzeio/AnalyzeIO.java
+++ src/main/java/net/xalcon/analyzeio/AnalyzeIO.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = AnalyzeIO.MODID, version = AnalyzeIO.VERSION, dependencies = "enderio@[1.10.2-3.1.190,)", acceptableRemoteVersions = "*")
+@Mod(modid = AnalyzeIO.MODID, version = AnalyzeIO.VERSION, dependencies = "required-after:EnderIO@[1.10.2-3.1.190,)", acceptableRemoteVersions = "*")
 public class AnalyzeIO
 {
     public static final String MODID = "analyzeio";
